434c8c98ec0ee934d92669e91cb2bcfcb01ca247
Erik Seferi <eseferi@c1r2s1.42wolfsburg.de>
2024-10-30 16:04:30 +0100

    first page done

---
+++ Transendence/src/views/Login.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import Form from '../components/Form.vue'
+</script>
+
+<template>
+  <main>
+      <h1>Login</h1>
+      <Form formType="login" />
+  </main>
+</template>
+
+<style scoped>
+    main {
+        display: flex;
+        flex-flow: column;
+        place-items: center;
+        place-content: center;
+        font-size: 1.2rem;
+        font-weight: 500;
+        margin-bottom: 0.4rem;
+        color: var(--color-text);
+    }
+</style>
---
+++ Transendence/src/views/Register.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import Form from '../components/Form.vue'
+</script>
+
+<template>
+  <main>
+      <h1>Register</h1>
+      <Form formType="register" />
+  </main>
+</template>
+
+<style scoped>
+    main {
+        display: flex;
+        flex-flow: column;
+        place-items: center;
+        place-content: center;
+        font-size: 1.2rem;
+        font-weight: 500;
+        margin-bottom: 0.4rem;
+        color: var(--color-text);
+    }
+</style>
