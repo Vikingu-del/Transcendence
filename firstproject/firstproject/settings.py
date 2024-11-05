631d4dd38cf7815b73f13a5ec27ed8c0fa1980b5
vanichx <vanyapetrunin88@gmail.com>
2024-11-05 18:37:08 +0100

    Add CustomUSer, Profile, Friend, Match

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -106,6 +106,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.CustomUser'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
