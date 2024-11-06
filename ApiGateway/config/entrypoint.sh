a9cfa2d6d625eff1aacb889ae5ba3a7d52fc5a6a
Erik Seferi <eseferi@c1r2s1.42wolfsburg.de>
2024-11-06 11:58:08 +0100

    trying to set up my project

--- ApiGateway/config/entrypoint.sh
+++ ApiGateway/config/entrypoint.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 set -e
 
-# Check if we should wait for backend services
-if [ "${WAIT_FOR_SERVICES}" == "true" ]; then
-    # Wait for backend services to be available
-    for service in ${BACKEND_SERVICES}; do
-        until nc -z "${service}" 8000; do
-            echo "Waiting for ${service} to be available..."
-            sleep 2
-        done
-    done
-else
-    echo "Skipping wait for backend services."
-fi
+# # Check if we should wait for backend services
+# if [ "${WAIT_FOR_SERVICES}" == "true" ]; then
+#     # Wait for backend services to be available
+#     for service in ${BACKEND_SERVICES}; do
+#         until nc -z "${service}" 8000; do
+#             echo "Waiting for ${service} to be available..."
+#             sleep 2
+#         done
+#     done
+# else
+#     echo "Skipping wait for backend services."
+# fi
 
-# Replace template file with environment variables
-envsubst < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+# Create a directory for SSL certificates
+mkdir -p /etc/nginx/ssl
+
+# Generate a self-signed certificate without prompting for input
+openssl req -newkey rsa:2048 -nodes -keyout /etc/nginx/ssl/nginx-selfsigned.key \
+  -x509 -days 365 -out /etc/nginx/ssl/nginx-selfsigned.crt \
+  -subj "/C=US/ST=State/L=City/O=Organization/OU=Department/CN=example.com"
 
 # Start NGINX
 nginx -g 'daemon off;'
